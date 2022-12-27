54cf78923303d8e6ed4d3feb0b148ca400389f59
shotasuzuki <shotasuzuki01@gmail.com>
2022-12-27 10:05:51 +0900

    1-3課題提出

---
+++ 1-3/main.js
@@ -0,0 +1,24 @@
+//問1_1
+let a ;
+console.log(a !==10);
+
+//問1_2
+let b;
+10<=b<=19 && b%2==0;
+
+//問2_1
+let x;
+if(10<=x<=20){
+    console.log("成功です");
+}else{
+    console.log("失敗ですjlla");
+}
+//問3
+let y=0;
+if(y%2==0){
+    console.log("偶数です");
+}else{
+    console.log("奇数です");
+}
+
+
